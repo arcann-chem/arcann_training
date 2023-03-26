88a53a9ca97fd6ed1e4904afa04b1041ed7309ef
Rolf David <12892294+cdavro@users.noreply.github.com>
2023-03-26 14:47:40 +0200

    Update list/test_list

--- deepmd_iterative/common/list.py
+++ deepmd_iterative/common/list.py
@@ -1,3 +1,8 @@
+"""
+Author: Rolf David
+Created: 2023/01/01
+Last modified: 2023/03/26
+"""
 # Standard library modules
 from typing import List
 
@@ -10,20 +15,35 @@ def remove_strings_containing_substring_in_list_of_strings(
     input_list: List[str], substring: str
 ) -> List[str]:
     """
-    Returns a new list of strings with all strings containing a given substring removed.
+    Remove all strings containing a given substring from a list of strings.
 
-    Args:
-        input_list (List[str]): The input list of strings.
-        substring (str): The substring to look for in the input strings.
+    Parameters
+    ----------
+    input_list : List[str]
+        The list of strings to remove the strings containing the substring.
+    substring : str
+        The substring to look for in the input strings.
 
-    Returns:
-        List[str]: The output list of strings with all the strings containing the given substring removed.
+    Returns
+    -------
+    List[str]
+        A new list of strings with all strings containing the given substring removed.
+    Raises
+    ------
+    TypeError
+        If input_list is not a list of strings, or substring is not a string.
+    ValueError
+        If input_list is empty.
     """
+    if not isinstance(input_list, list) or not isinstance(substring, str):
+        raise TypeError(
+            "Invalid input type. input_list must be a list of strings and substring must be a string."
+        )
 
-    # Use a list comprehension to create a new list with the updated strings
+    if len(input_list) == 0:
+        raise ValueError("input_list must not be empty.")
+
     output_list = [string.strip() for string in input_list if substring not in string]
-
-    # Return the updated list
     return output_list
 
 
@@ -33,21 +53,39 @@ def replace_substring_in_list_of_strings(
     input_list: List[str], substring_in: str, substring_out: str
 ) -> List[str]:
     """
-    Replaces a specified substring with a new substring in each string in a given list.
+    Replaces a specified substring with a new substring in each string of a list.
 
-    Args:
-        input_list (List[str]): A list of input strings.
-        substring_in (str): The substring to replace in the input strings.
-        substring_out (str): The new substring to replace with.
+    Parameters
+    ----------
+    input_list : List[str]
+        A list of input strings.
+    substring_in : str
+        The substring to replace in the input strings.
+    substring_out : str
+        The new substring to replace with.
 
-    Returns:
-        List[str]: A list of output strings with the specified substring replaced by the new substring.
+    Returns
+    -------
+    List[str]
+        A list of output strings with the specified substring replaced by the new substring.
+
+    Raises
+    ------
+    TypeError
+        If input_list is not a list of strings.
+    ValueError
+        If substring_in or substring_out is an empty string.
     """
+    if not isinstance(input_list, list):
+        raise TypeError("Invalid input type. input_list must be a list of strings.")
 
-    # Use a list comprehension to create a new list with the updated strings
+    if not substring_in:
+        raise ValueError("Invalid input. substring_in must be a non-empty string.")
+
+    if not substring_out:
+        raise ValueError("Invalid input. substring_out must be a non-empty string.")
+
     output_list = [
         string.replace(substring_in, substring_out).strip() for string in input_list
     ]
-
-    # Return the updated list
     return output_list
--- deepmd_iterative/tests/test_list.py
+++ deepmd_iterative/tests/test_list.py
@@ -1,8 +1,13 @@
-from pathlib import Path
+"""
+Author: Rolf David
+Created: 2023/01/01
+Last modified: 2023/03/26
+"""
+# Standard library modules
 import unittest
 import tempfile
 
-# deepmd_iterative imports
+# Local imports
 from deepmd_iterative.common.list import (
     remove_strings_containing_substring_in_list_of_strings,
     replace_substring_in_list_of_strings,
@@ -10,67 +15,152 @@ from deepmd_iterative.common.list import (
 
 
 class TestRemoveStringsContainingSubstringInList(unittest.TestCase):
+    """
+    Test case for the remove_strings_containing_substring_in_list_of_strings() function.
+
+    Methods
+    -------
+    test_remove_strings_containing_substring_in_list_of_strings():
+        Test the remove_strings_containing_substring_in_list_of_strings() function with valid input.
+    test_remove_strings_containing_substring_in_list_of_strings_empty_list():
+        Test the remove_strings_containing_substring_in_list_of_strings() function with an empty input list.
+    test_remove_strings_containing_substring_in_list_of_strings_invalid_input():
+        Test the remove_strings_containing_substring_in_list_of_strings() function with invalid input types.
+    """
+
     def setUp(self):
-        self.input_list = ["hello", "world", "hello world", "goodbye"]
-        self.substring = "hello"
+        self.input_list = [
+            "quantum mechanics",
+            "chemical kinetics",
+            "thermodynamics",
+            "quantum chemistry",
+            "chemical equilibrium",
+        ]
+        self.substring = "quantum"
 
     def tearDown(self):
         pass
 
-    def test_remove_strings_containing_substring(self):
-        output_list = remove_strings_containing_substring_in_list_of_strings(
+    def test_remove_strings_containing_substring_in_list_of_strings(self):
+        expected_output = [
+            "chemical kinetics",
+            "thermodynamics",
+            "chemical equilibrium",
+        ]
+        output = remove_strings_containing_substring_in_list_of_strings(
             self.input_list, self.substring
         )
-        self.assertListEqual(output_list, ["world", "goodbye"])
+        self.assertEqual(output, expected_output)
+
+    def test_remove_strings_containing_substring_in_list_of_strings_empty_list(self):
+        self.input_list = []
+        with self.assertRaises(ValueError):
+            remove_strings_containing_substring_in_list_of_strings(
+                self.input_list, self.substring
+            )
+
+    def test_remove_strings_containing_substring_in_list_of_strings_invalid_input(self):
+        self.input_list = "not a list"
+        with self.assertRaises(TypeError):
+            remove_strings_containing_substring_in_list_of_strings(
+                self.input_list, self.substring
+            )
+
+        self.input_list = [
+            "quantum mechanics",
+            "chemical kinetics",
+            "thermodynamics",
+            "quantum chemistry",
+            "chemical equilibrium",
+        ]
+        self.substring = 10
+        with self.assertRaises(TypeError):
+            remove_strings_containing_substring_in_list_of_strings(
+                self.input_list, self.substring
+            )
 
 
 class TestReplaceSubstringInList(unittest.TestCase):
-    def setUp(self):
-        # Create a list of input strings to use in the tests
-        self.input_list = ["hello world", "foo bar", "baz qux"]
+    """
+    Test case for the replace_substring_in_list_of_strings() function.
 
-        # Create a temporary file to use in the tests
+    Methods
+    -------
+    test_replace_substring_in_list_of_strings():
+        Test the function with a list of strings and check that it replaces the specified substring correctly.
+    test_replace_substring_in_list_of_strings_invalid_input():
+        Test the function with an invalid input and check that it raises a TypeError.
+    test_replace_substring_in_list_of_strings_empty_substring():
+        Test the function with an empty substring and check that it raises a ValueError.
+    test_replace_substring_in_list_with_temp_file():
+        Test the function with a file object and check that it replaces the specified substring correctly.
+    """
+
+    def setUp(self):
+        self.input_list = [
+            "quantum mechanics",
+            "chemical kinetics",
+            "thermodynamics",
+            "quantum chemistry",
+            "chemical equilibrium",
+        ]
+        self.substring_in = "quantum"
+        self.substring_out = "classical"
         self.tmp_file = tempfile.NamedTemporaryFile()
 
     def tearDown(self):
-        # Delete the temporary file
         self.tmp_file.close()
 
-    def test_replace_substring_in_list(self):
-        # Call the function with some test inputs
-        output_list = replace_substring_in_list_of_strings(self.input_list, "o", "O")
+    def test_replace_substring_in_list_of_strings(self):
+        expected_output = [
+            "classical mechanics",
+            "chemical kinetics",
+            "thermodynamics",
+            "classical chemistry",
+            "chemical equilibrium",
+        ]
+        output = replace_substring_in_list_of_strings(
+            self.input_list, self.substring_in, self.substring_out
+        )
+        self.assertEqual(output, expected_output)
 
-        # Check that the output list has the correct length
-        self.assertEqual(len(output_list), len(self.input_list))
+    def test_replace_substring_in_list_of_strings_invalid_input(self):
+        input_list = "not a list"
+        with self.assertRaises(TypeError):
+            replace_substring_in_list_of_strings(
+                input_list, self.substring_in, self.substring_out
+            )
 
-        # Check that each string in the output list has the specified substring replaced
-        self.assertEqual(output_list[0], "hellO wOrld")
-        self.assertEqual(output_list[1], "fOO bar")
-        self.assertEqual(output_list[2], "baz qux")
+    def test_replace_substring_in_list_of_strings_empty_substring(self):
+        substring_in = ""
+        with self.assertRaises(ValueError):
+            replace_substring_in_list_of_strings(
+                self.input_list, substring_in, self.substring_out
+            )
 
-    def test_replace_substring_in_list_with_empty_input_list(self):
-        # Call the function with an empty input list
-        output_list = replace_substring_in_list_of_strings([], "o", "O")
-
-        # Check that the output list is also empty
-        self.assertEqual(len(output_list), 0)
+        substring_out = ""
+        with self.assertRaises(ValueError):
+            replace_substring_in_list_of_strings(
+                self.input_list, self.substring_in, substring_out
+            )
 
     def test_replace_substring_in_list_with_temp_file(self):
-        # Write the input list to the temporary file
         with open(self.tmp_file.name, "w") as f:
             f.write("\n".join(self.input_list))
 
-        # Call the function with the contents of the temporary file
         with open(self.tmp_file.name, "r") as f:
-            output_list = replace_substring_in_list_of_strings(f.readlines(), "o", "O")
+            output = replace_substring_in_list_of_strings(
+                f.readlines(), self.substring_in, self.substring_out
+            )
 
-        # Check that the output list has the correct length
-        self.assertEqual(len(output_list), len(self.input_list))
-
-        # Check that each string in the output list has the specified substring replaced
-        self.assertEqual(output_list[0], "hellO wOrld")
-        self.assertEqual(output_list[1], "fOO bar")
-        self.assertEqual(output_list[2], "baz qux")
+        expected_output = [
+            "classical mechanics",
+            "chemical kinetics",
+            "thermodynamics",
+            "classical chemistry",
+            "chemical equilibrium",
+        ]
+        self.assertEqual(output, expected_output)
 
 
 if __name__ == "__main__":
