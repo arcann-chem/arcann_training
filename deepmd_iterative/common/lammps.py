03f49dc4418718f364fd33ede5bd7d96eb31aaad
Rolf David <12892294+cdavro@users.noreply.github.com>
2023-09-05 11:42:39 +0200

    added lammps unittests

--- deepmd_iterative/common/lammps.py
+++ deepmd_iterative/common/lammps.py
@@ -6,7 +6,7 @@
 #   SPDX-License-Identifier: AGPL-3.0-only                                                           #
 #----------------------------------------------------------------------------------------------------#
 Created: 2022/01/01
-Last modified: 2023/08/16
+Last modified: 2023/09/05
 
 The lammps module provides functions to manipulate LAMMPS data (as list of strings).
 
@@ -48,7 +48,13 @@ def read_lammps_data(
 
     Raises
     ------
+    ValueError
+        If any required information is missing or inconsistent in the input data.
     """
+    # Input validation
+    if not lines or not isinstance(lines, list):
+        raise ValueError("Input 'lines' must be a non-empty list of strings.")
+    
     # Initialize variables
     num_atoms = None
     num_atom_types = None
@@ -67,29 +73,68 @@ def read_lammps_data(
             continue
         if "Atoms" in line:
             in_atoms_section = True
+            in_masses_section = False
             continue
         if in_atoms_section:
-            atoms.append(line.split()[3:6])
+            atoms.append(line.split()[2:6])
+            continue
         if "Masses" in line:
             in_masses_section = True
+            in_atoms_section = False
             continue
         if in_masses_section:
             fields = line.split()
             masses[int(fields[0])] = float(fields[1])
+            continue
         if "xlo" in line:
             xlo, xhi = map(float, line.split()[0:2])
+            continue
         if "ylo" in line:
             ylo, yhi = map(float, line.split()[0:2])
+            continue
         if "zlo" in line:
             zlo, zhi = map(float, line.split()[0:2])
+            continue
         if "xy" in line:
             xy, xz, yz = map(float, line.split()[0:3])
-        if "atome type" in line:
+            continue
+        if "atom types" in line:
             num_atom_types = int(line.split()[0])
+            continue
         if "atoms" in line:
             num_atoms = int(line.split()[0])
+            continue
     # Convert lists to NumPy arrays
     atoms = np.array(atoms, dtype=float)
+    if num_atoms == None:
+        error_msg = "The number of atoms was not found."
+        raise ValueError(error_msg)
+    if num_atom_types == None:
+        error_msg = "The number of atom types was not found."
+        raise ValueError(error_msg)
+    if (
+        xlo == None
+        or xhi == None
+        or ylo == None
+        or yhi == None
+        or zlo == None
+        or zhi == None
+    ):
+        error_msg = f"Invalid box coordinates."
+        raise ValueError(error_msg)
+    if len(masses) == 0:
+        error_msg = f"Masses not found"
+        raise ValueError(error_msg)
+    if len(masses) != num_atom_types:
+        error_msg = f"Number of masses ('{len(masses)}') does not match the number of atom types ('{num_atom_types}')."
+        raise ValueError(error_msg)
+    if len(atoms) == 0:
+        error_msg = f"Coordinates not found."
+        raise ValueError(error_msg)
+    if len(atoms) != num_atoms:
+        error_msg = f"Number of coordinates ('{len(atoms)}') does not match the number of atoms ('{num_atoms}')."
+        raise ValueError(error_msg)
+
     # Return results
     return (
         num_atoms,
