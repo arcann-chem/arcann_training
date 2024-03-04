18b405aff9e3f92225b706b347309096195009ed
Rolf David <12892294+cdavro@users.noreply.github.com>
2024-03-04 14:36:22 +0100

    Fix #126

--- deepmd_iterative/exploration/check.py
+++ deepmd_iterative/exploration/check.py
@@ -6,7 +6,7 @@
 #   SPDX-License-Identifier: AGPL-3.0-only                                                           #
 #----------------------------------------------------------------------------------------------------#
 Created: 2022/01/01
-Last modified: 2023/10/13
+Last modified: 2024/03/04
 """
 # Standard library modules
 import logging
@@ -105,18 +105,7 @@ def main(
                     )
                     if lammps_output_file.is_file():
                         lammps_output = textfile_to_string_list(lammps_output_file)
-                        if any("Total wall time:" in f for f in lammps_output):
-                            system_count += 1
-                            completed_count += 1
-                            exploration_json["systems_auto"][system_auto][
-                                "completed_count"
-                            ] += 1
-                            timings_str = [
-                                zzz for zzz in lammps_output if "Loop time of" in zzz
-                            ]
-                            timings.append(float(timings_str[0].split(" ")[3]))
-                            del timings_str
-                        elif (local_path / "skip").is_file():
+                        if (local_path / "skip").is_file():
                             skipped_count += 1
                             exploration_json["systems_auto"][system_auto][
                                 "skipped_count"
@@ -128,6 +117,17 @@ def main(
                                 "forced_count"
                             ] += 1
                             logging.warning(f"'{lammps_output_file}' forced.")
+                        elif any("Total wall time:" in f for f in lammps_output):
+                            system_count += 1
+                            completed_count += 1
+                            exploration_json["systems_auto"][system_auto][
+                                "completed_count"
+                            ] += 1
+                            timings_str = [
+                                zzz for zzz in lammps_output if "Loop time of" in zzz
+                            ]
+                            timings.append(float(timings_str[0].split(" ")[3]))
+                            del timings_str
                         else:
                             logging.critical(
                                 f"'{lammps_output_file}' failed. Check manually."
@@ -156,7 +156,19 @@ def main(
                     )
                     if ipi_output_file.is_file():
                         ipi_output = textfile_to_string_list(ipi_output_file)
-                        if any("SIMULATION: Exiting cleanly" in f for f in ipi_output):
+                        if (local_path / "skip").is_file():
+                            skipped_count += 1
+                            exploration_json["systems_auto"][system_auto][
+                                "skipped_count"
+                            ] += 1
+                            logging.warning(f"'{ipi_output_file}' skipped.")
+                        elif (local_path / "force").is_file():
+                            forced_count += 1
+                            exploration_json["systems_auto"][system_auto][
+                                "forced_count"
+                            ] += 1
+                            logging.warning(f"'{ipi_output_file}' forced.")
+                        elif any("SIMULATION: Exiting cleanly" in f for f in ipi_output):
                             system_count += 1
                             completed_count += 1
                             exploration_json["systems_auto"][system_auto][
@@ -175,18 +187,6 @@ def main(
                                 np.average(np.asarray(ipi_time2, dtype="float32"))
                             )
                             del ipi_time, ipi_time2
-                        elif (local_path / "skip").is_file():
-                            skipped_count += 1
-                            exploration_json["systems_auto"][system_auto][
-                                "skipped_count"
-                            ] += 1
-                            logging.warning(f"'{ipi_output_file}' skipped.")
-                        elif (local_path / "force").is_file():
-                            forced_count += 1
-                            exploration_json["systems_auto"][system_auto][
-                                "forced_count"
-                            ] += 1
-                            logging.warning(f"'{ipi_output_file}' forced.")
                         else:
                             logging.critical(
                                 f"'{ipi_output_file}' failed. Check manually."
