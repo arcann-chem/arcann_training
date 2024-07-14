4dee74442c8bf821b8098b7f1840e9c7f5559da1
Rolf David <12892294+cdavro@users.noreply.github.com>
2024-07-14 15:11:54 +0200

    Fix bug if forced but no crossing of sigma high

--- arcann_training/exploration/deviate.py
+++ arcann_training/exploration/deviate.py
@@ -6,7 +6,7 @@
 #   SPDX-License-Identifier: AGPL-3.0-only                                                           #
 #----------------------------------------------------------------------------------------------------#
 Created: 2022/01/01
-Last modified: 2024/05/15
+Last modified: 2024/07/14
 """
 
 # Standard library modules
@@ -211,14 +211,12 @@ def main(
                         arcann_logger.error("Aborting...")
                         return 1
 
-                    end_row_number = get_last_frame_number(
-                        model_deviation,
-                        sigma_high_limit,
-                        exploration_json["systems_auto"][system_auto]["disturbed_start"],
-                    )
-                    arcann_logger.debug(f"end_row_number: {end_row_number}, start_row_number: {start_row_number}")
+                    end_row_number = get_last_frame_number(model_deviation, sigma_high_limit, exploration_json["systems_auto"][system_auto]["disturbed_start"])
+
                     if (local_path / "force").is_file():
-                        end_row_number = end_row_number - 1
+                        end_row_number = model_deviation.shape[0] - 1
+
+                    arcann_logger.debug(f"end_row_number: {end_row_number}, start_row_number: {start_row_number}")
 
                     if exploration_json["systems_auto"][system_auto]["exploration_type"] == "lammps" or exploration_json["systems_auto"][system_auto]["exploration_type"] == "i-PI":
 
